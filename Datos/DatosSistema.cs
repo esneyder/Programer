da87ebeb3323af1282695652d1757a4a197f6acb
esneyder <edesalla17@gmail.com>
2014-09-11 11:53:12 -0500

    Leermos los datos pertinentes

--- Datos/DatosSistema.cs
+++ Datos/DatosSistema.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,6 +10,14 @@ namespace Datos
 {
    public class DatosSistema
     {
-
+       public DataTable getDatos(string procedimiento,
+           string[] parametros, params object[] valparametros) {
+               Conexion con = new Conexion();
+               SqlCommand cmd = new SqlCommand();
+               cmd.Connection = con.conectar();
+               cmd.CommandText = procedimiento;
+               cmd.CommandType = CommandType.StoredProcedure;
+       
+       }
     }
 }
