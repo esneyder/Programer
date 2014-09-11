d2b2a2929c8645134ee7fc77c5680283deb77438
esneyder <edesalla17@gmail.com>
2014-09-11 11:47:24 -0500

    Implementada la clase conexión

--- Datos/Conexion.cs
+++ Datos/Conexion.cs
@@ -2,11 +2,43 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Data.SqlClient;
 using System.Threading.Tasks;
 
 namespace Datos
 {
     public class Conexion
     {
+
+        public SqlConnection con { get; set; }
+
+        private string cadenaconexion() {
+            return @"data source=.;initial catalog=bd;user id=esneyder;password=alvarez123";
+        }
+
+        //método conexión
+        public SqlConnection conectar() {
+            con = new SqlConnection(cadenaconexion());
+            try
+            {
+                this.con.Open();
+                return this.con;
+            }
+            catch (Exception)
+            {
+
+                return null;
+            }
+        
+
+        }
+        //método para cerrar la conexión
+        public void desconectar(){
+            if (this.con!=null)
+            {
+                this.con.Close();
+            }
+        }
+
     }
 }
